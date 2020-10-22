de0d7b5a066772db7a3c0aa96c04b436a8669bbd
dib1000 <dib5017@gmail.com>
2020-10-21 22:12:43 -0400

    I tested this program and it works now!

--- Demo.java
+++ Demo.java
@@ -1,4 +1,40 @@
 public class Demo {
+  public static String arrtoString(int[] arr) {
+    if (arr.length == 1) {
+    return "{"+arr[0]+"}";
+  }
+  String array = "{";
+  for(int i = 0; i<arr.length; i++) {
+    if(i==arr.length-1) {
+      array = array + " " + arr[i];
+    }
+    else if(i==0) {
+      array = array + arr[i] + ",";
+    }
+    else {
+      array = array + " " + arr[i] +",";
+    }
+  }
+  return array + "}";
+  }
+  public static String arrayDeeptoString(int[][] arr) {
+    if (arr.length == 1) {
+    return "{"+arrtoString(arr[0])+"}";
+  }
+  String array = "{";
+  for(int i = 0; i<arr.length; i++) {
+    if(i==arr.length-1) {
+      array = array + " " + arrtoString(arr[i]);
+    }
+    else if(i==0) {
+      array = array + arrtoString(arr[i]) + ",";
+    }
+    else {
+      array = array + " " + arrtoString(arr[i]) +",";
+    }
+  }
+  return array + "}";
+  }
   public static void printLoop(int n){
   for(int i = 1; i<=n; i++){
     for(int j = 0; j<(n+1)-i; j++){
@@ -15,5 +51,7 @@ public class Demo {
       int a = Integer.parseInt(args[0]);
       printLoop(a);
     }
+  //int [][] test = {{2, 0}};
+  //System.out.println(arrayDeeptoString(test));
   }
 }
